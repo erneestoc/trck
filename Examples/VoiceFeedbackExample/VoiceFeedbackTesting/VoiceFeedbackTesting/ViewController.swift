3ff78a9879d3a8943719362e85b4e2a2563693bb
Ernesto Cambuston <e.cambuston@gmail.com>
2018-02-06 15:20:20 -0800

    🚀 Audio ducking working correctly on device

--- Examples/VoiceFeedbackExample/VoiceFeedbackTesting/VoiceFeedbackTesting/ViewController.swift
+++ Examples/VoiceFeedbackExample/VoiceFeedbackTesting/VoiceFeedbackTesting/ViewController.swift
@@ -9,53 +9,80 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    private var currentLocale = Locale.current.identifier
-    private var currentSpeaker:TrckFeedbackProtocol?
-    @IBOutlet weak var speakButton: UIButton!
-    @IBOutlet weak var distanceTextField: UITextField!
-    @IBOutlet weak var timeTextField: UITextField!
-    @IBOutlet weak var languagePickerView: UIPickerView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        languagePickerView.dataSource = self
-        languagePickerView.delegate = self
+  
+  private let speaker = TrckSpeaker()
+  private var currentLocale = Locale.current.identifier
+  private var currentSpeaker:TrckFeedbackProtocol?
+  @IBOutlet weak var speakButton: UIButton!
+  @IBOutlet weak var distanceTextField: UITextField!
+  @IBOutlet weak var timeTextField: UITextField!
+  @IBOutlet weak var languagePickerView: UIPickerView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    languagePickerView.dataSource = self
+    languagePickerView.delegate = self
+    distanceTextField.delegate = self
+    timeTextField.delegate = self
+    distanceTextField.tag = 0
+    timeTextField.tag = 1
+    setLanguage(currentLocale)
+  }
+  
+  private func setLanguage(_ code:String) {
+    let setup = TrckSetup(unitSystem: .metric, voiceFeedback: .regular, halfsUnits: 2)
+    currentSpeaker = TrckEnglishFeedback(setup: setup)
+  }
+  
+  @IBAction func speak(_ sender: Any) {
+    print("[Speech] - Checking text exists")
+    guard let distanceText = distanceTextField.text,
+      let timeText = timeTextField.text else {return}
+    print("[Speech] - Parsing Text distance:\(distanceText) \n\t\t\ttime: \(timeText)")
+    if let distance = Double(distanceText),
+      let time = Int(timeText) {
+      if let speech = currentSpeaker?.feedbackFor(distance: distance,
+                                                  time: time,
+                                                  pace: Double(time) / distance) {
+        saySpeech(speech)
+      }
     }
-    
-    @IBAction func speak(_ sender: Any) {
-        guard let distanceText = distanceTextField.text,
-            let timeText = timeTextField.text else {return}
-        if let distance = Double(distanceText),
-            let time = Int(timeText) {
-            if let speech = currentSpeaker?.feedbackFor(distance: distance,
-                                                        time: time,
-                                                        pace: Double(time) / distance) {
-                saySpeech(speech)
-            }
-        }
+  }
+  
+  private func saySpeech(_ speech:String) {
+    print("[Speech] - \(speech)")
+    speaker.speak(string: speech, language: currentLocale)
+  }
+  
+}
+
+extension ViewController: UITextFieldDelegate {
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    if textField.tag == 0 {
+      if let field = view.viewWithTag(1) as? UITextField {
+        field.becomeFirstResponder()
+      }
+    } else if textField.tag == 1 {
+      textField.resignFirstResponder()
     }
-    
-    private func saySpeech(_ speech:String) {
-        
-    }
-    
+    return false
+  }
 }
 
 extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return Locale.availableIdentifiers.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return Locale.availableIdentifiers[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        currentLocale = Locale.availableIdentifiers[row]
-    }
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    return 1
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    return Locale.availableIdentifiers.count
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    return Locale.availableIdentifiers[row]
+  }
+  
+  func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    currentLocale = Locale.availableIdentifiers[row]
+  }
 }
---
+++ Sources/Trck/languages/TrckSpeaker.swift
@@ -0,0 +1,73 @@
+import AVFoundation
+
+class TrckSpeaker:NSObject {
+  private typealias TrckSpeech = (speech:String, language:String)
+  private let speechSynthesizer = AVSpeechSynthesizer()
+  private var audioSession = AVAudioSession.sharedInstance()
+  private let utteranceRate = AVSpeechUtteranceDefaultSpeechRate
+  private var speechQueue = [TrckSpeech]()
+  override init() {
+    super.init()
+    speechSynthesizer.delegate = self
+  }
+  public func speak(string:String, language:String) {
+    speechQueue.append((string, language))
+    flush()
+  }
+
+  private func flush() {
+    if !speechSynthesizer.isSpeaking {
+      startDucking()
+      sayNext()
+    }
+  }
+
+  private func sayNext() {
+    let nextSpeech = speechQueue.remove(at: 0)
+    speechSynthesizer.stopSpeaking(at: .immediate)
+    let utterance = getUtteranceFor(nextSpeech)
+    speechSynthesizer.speak(utterance)
+  }
+
+  private func getUtteranceFor(_ speech:TrckSpeech) -> AVSpeechUtterance {
+    let utterance = AVSpeechUtterance(string: speech.0)
+    utterance.voice = AVSpeechSynthesisVoice(language: speech.1)
+    utterance.rate = utteranceRate
+    utterance.preUtteranceDelay = 0.5
+    utterance.postUtteranceDelay = 0.5
+    return utterance
+  }
+
+  private func startDucking() {
+    audioSession = AVAudioSession.sharedInstance()
+    print("[TrckSpeaker] - start audio ducking")
+    do {
+      try audioSession.setCategory(AVAudioSessionCategoryPlayback, with: .duckOthers)
+      try audioSession.setActive(true)
+    } catch let error as NSError {
+      print(error)
+    }
+  }
+  private func stopDucking() {
+    print("[TrckSpeaker] - stop audio ducking")
+    do {
+      try audioSession.setCategory(AVAudioSessionCategoryPlayback, with: .mixWithOthers)
+      try audioSession.setActive(false)
+    } catch let error as NSError {
+      print(error)
+    }
+  }
+}
+
+extension TrckSpeaker: AVSpeechSynthesizerDelegate {
+  func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+    if speechQueue.isEmpty {
+      let when = DispatchTime.now() + 1
+      DispatchQueue.main.asyncAfter(deadline: when) { [weak self] in
+        self?.stopDucking()
+      }
+    } else {
+      sayNext()
+    }
+  }
+}
